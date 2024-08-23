c445eda6b5bd7495ef5b20b3b19be6e0b3cb8c32
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-08-23 11:57:52 +0530

    added Customer Entity

---
+++ src/main/java/org/example/demo/entity/Customer.java
@@ -0,0 +1,17 @@
+package org.example.demo.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    private String  id;
+    private String name;
+    private String address;
+    private int phone;
+
+}
